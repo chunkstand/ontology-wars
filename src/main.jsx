cf2c84aeff70165787d36c56594197f7815a986f
chunkstand <chunkstand@proton.me>
2026-03-01 19:06:01 -0700

    feat: add Vite + React setup for GitHub Pages deployment

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { OntologyWars } from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <OntologyWars />
+  </React.StrictMode>,
+)
